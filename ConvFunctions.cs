c9742fbe0b2893436ae39dc2989f7d0bc1c4ed3c
Jens Eckervogt <57066679+DeafMan1983@users.noreply.github.com>
2023-05-12 20:27:04 +0200

    Update ConvFunctions.cs

--- ConvFunctions.cs
+++ ConvFunctions.cs
@@ -1,24 +1,27 @@
-namespace DeafMan1983;
+﻿namespace DeafMan1983;
 
 using System.Runtime.InteropServices;
 using System.Text;
+
 public unsafe static class ConvFunctions
 {
     //
     //  Conversion functions for sbyte * and string hacks!
     //
     //  Changvs and fixes:
+    //  Fixed:
+    //      SByteDoublePointersWithStringArray, thanks Jester @QubitTooLate
+    //      
     //  -----------------------------------------------------
     //  Replaced:
     //      StringFromCharPtr to SBytePointerWithStringForAdvanced
     //      StringFromHeap to SBytePointerWithString
     //      CharPtrToString to StringwithSBytePointer
-    //  Added:
-    //  new functions for sbyte ** and string[]
-    //      SByteDoublePointersWithStringArray ( Fixed and add NativeMemory, Thanks @jkotas ( Jan Kotas!!! ) )
+    //  Added: new functions for sbyte ** and string[]
+    //      SByteDoublePointersWithStringArray ( Fixed and add NativeMemory, Thanks Jan Kotas!!! )
     //      StringArrayWithSByteDoublePointers
-    //  Update: to 2.0.0
-     
+    //  Update: to 2.0.1
+
 
     public static int StringOfSize(string str_value)
     {
@@ -74,19 +77,95 @@ public unsafe static class ConvFunctions
         return Encoding.UTF8.GetString((byte *)charptr, (int)(ptr - (byte *)charptr));
     }
 
-    public static sbyte **SByteDoublePointersWithStringArray(string[] args)
+    /*
+        Woah it works fine, thanks Jester @QubitTooLate!!!
+    */
+    internal class UnmanagedUTF8StringArray : IDisposable
     {
-        sbyte** sArgs = (sbyte **)NativeMemory.Alloc((nuint)args.Length * (nuint)sizeof(sbyte*));
-        for (int i = 0; i < args.Length; i++)
+        private sbyte **__values;
+        private int __length;
+        private bool disposed = false;
+
+        public UnmanagedUTF8StringArray(string[] arrays)
         {
-            sArgs[i] = SBytePointerWithString(args[i]);
+            __values = (sbyte **)NativeMemory.Alloc((nuint)arrays.Length, (nuint)sizeof(sbyte *));
+            __length = arrays.Length;
+
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                int len = Encoding.UTF8.GetByteCount(arrays[i]) + 1;
+                __values[i] = (sbyte *)NativeMemory.Alloc((nuint)len);
+                Encoding.UTF8.GetBytes(arrays[i].AsSpan(), new Span<byte>(__values[i], len));
+                __values[i][len - 1] = 0;
+            }
         }
 
-        return sArgs;
+        ~UnmanagedUTF8StringArray()
+        {
+            Dispose(false);
+        }
+
+         private void Dispose(bool disposing)
+        {
+            // Check to see if Dispose has already been called.
+            if(!this.disposed)
+            {
+                if(disposing)
+                {
+                    if (__values != null)
+                    {
+                        for (int i = 0; i < __length; i++)
+                        {
+                            if (__values[i] != null)
+                            {
+                                NativeMemory.Free(__values[i]);
+                                __values[i] = null;
+                            }
+                        }
+                    }
+                }
+
+                NativeMemory.Free(__values);
+                __values = null;
+
+                disposed = true;
+
+            }
+        }
+
+        public sbyte **Valves => __values;
+        public int Length => __length;
+        public sbyte *this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= __length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return __values[index];
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+
+    public static sbyte **SByteDoublePointersWithStringArray(string[] args)
+    {
+        var um = new UnmanagedUTF8StringArray(args);
+        return um.Valves;
     }
 
     public static string[] StringArrayWithSByteDoublePointers(sbyte **sArrays, int array_length)
     {
+        if (array_length < 0)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
         string[] arrays = new string[array_length];
         for (int i = 0; i < array_length; i++)
         {
